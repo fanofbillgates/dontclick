d623fb408a04ec4486a2dcd91bf528ba26c8a1da
гилл бейтс <115172411+fanofbillgates@users.noreply.github.com>
2024-04-07 17:26:14 +0600

    Commit

--- shingshong.py
+++ shingshong.py
@@ -1,1 +1,74 @@
-print('hola world!!111')
+import pygame
+pygame.init()
+
+WINDOW_SIZE = (700, 500)
+FPS = 60
+game = True
+#BACKGROUND_IMAGE = "background.jpg"
+
+window = pygame.display.set_mode(WINDOW_SIZE)
+window.fill((123,123,123))
+pygame.display.set_caption("PING PONG")
+clock = pygame.time.Clock()
+#background = pygame.transform.scale(pygame.image.load(BACKGROUND_IMAGE), WINDOW_SIZE)
+
+class DefaultSprite():
+
+    def __init__(self, x, y, width, height, filename):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.filename = filename
+        self.image = pygame.transform.scale(pygame.image.load(filename), (width, height))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    
+    def update(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Rocket1(DefaultSprite):
+    def __init__(self, x, y, width, height, filename, speed):
+        super().__init__(x, y, width, height, filename)
+        self.speed = speed
+        
+class Ball(DefaultSprite):
+
+    def __init__(self, x, y, width, height, filename, speed):
+        super().__init__(x, y, width, height, filename)
+        self.speed = speed
+        self.rotate = 60
+    
+    def move(self):
+
+        if self.rotate == 60:
+            self.rect.x += self.speed
+            self.rect.y += self.speed
+        elif self.rotate == 240:
+            self.rect.x -= self.speed
+            self.rect.y -= self.speed
+
+        if self.rect.y >= WINDOW_SIZE[1]:
+            self.rotate = 240
+
+        if self.rect.y <= 0:
+            self.rotate = 60
+
+ball = Ball(0, 0, 50, 50, "ball.jpg", 1)
+rocket1 = Rocket1(10,10,50,50, 'rocket1.png', 1)
+rocket2 = Rocket1(200,10,50,50, 'rocket2.png', 1)
+
+while game:
+    clock.tick(FPS)
+
+    ball.update()
+    rocket1.update()
+    keys = pygame.key.get_pressed()
+    ball.move()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+    
+    pygame.display.update()
